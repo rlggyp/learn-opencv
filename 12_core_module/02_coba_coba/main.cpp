e864f2273ed692e6bb531fe0a2c5a47a99861967
Erlangga Yudi Pradana <erlanggayudipradana@gmail.com>
2023-03-19 21:09:17 +0700

    add: canny threshold

--- 12_core_module/02_coba_coba/main.cpp
+++ 12_core_module/02_coba_coba/main.cpp
@@ -6,41 +6,42 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
-cv::Mat img, output, resize;
+cv::Mat img, output, resized;
 int threshold1=0, threshold1Max=1000, threshold2=0, threshold2Max=1000;
 
 void on_trackbar(int, void*)
 {
-  cv::Canny(img, output, threshold1, threshold2);
+  cv::Canny(resized, output, threshold1, threshold2);
   imshow("Canny", output);
 }
 
 int main(int agrc, char *argv[])
 { 
+  std::string save_path = argv[1];
+  save_path =  save_path + "_canny.jpg";
   img = cv::imread(argv[1]);
   char key = 0;
 
   if (img.empty())
     return 1;
 
-  // imshow("Image", img);
+  cv::resize(img, resized, cv::Size(), 0.25, 0.25);
+  cv::Canny(resized, output, threshold1, threshold2);
+  imshow("Canny", output);
 
-  cv::Canny(img, output, threshold1, threshold2);
-  int rows, cols;
-  // rows = img.rows/4;
-  // cols = img.cols/4;
-  // resize = cv::Size(rows, cols);
-  
-  // imshow("Canny", output);
-  cv::namedWindow("Canny1", cv::WINDOW_NORMAL);
-  cv::createTrackbar("CannyThreshold_1", "Canny1", &threshold1, threshold1Max, on_trackbar);
-  cv::createTrackbar("CannyThreshold_2", "Canny1", &threshold2, threshold2Max, on_trackbar);
+  cv::namedWindow("Canny", cv::WINDOW_NORMAL);
+  cv::createTrackbar("CannyThreshold_1", "Canny", &threshold1, threshold1Max, on_trackbar);
+  cv::createTrackbar("CannyThreshold_2", "Canny", &threshold2, threshold2Max, on_trackbar);
 
   while (key != (char)32)
   {
     key = cv::waitKey(100);
     if (key == 'w')
-      cv::imwrite("hasilCanny", output);
+    {
+      cv::Canny(img, output, threshold1, threshold2);
+      cv::imwrite(save_path, output);
+      printf("Successfully saved in %s", save_path);
+    }
   }
 
   img.release();
